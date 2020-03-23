644baffb9d5d9ceb9a0926563e7dc0bd98a3c193
Sébastien Piquemal <sebpiq@gmail.com>
2020-03-23 18:38:50 +0100

    added controls, broken so far

--- src/render.test.ts
+++ src/render.test.ts
@@ -1,54 +1,43 @@
+/*
+ * Copyright (c) 2012-2020 Sébastien Piquemal <sebpiq@gmail.com>
+ *
+ * BSD Simplified License.
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ *
+ * See https://github.com/sebpiq/WebPd_pd-renderer for documentation
+ *
+ */
+
 import assert from 'assert'
 import render from './render'
 import TEST_PATCHES from '@webpd/shared/test-patches'
+import parse from '@webpd/pd-parser/src/parse'
 const NEWLINE_REGEX = /\r?\n/
 
 describe('render', () => {
-    it('should succeed parsing/rendering all test patches identically', () => {
-        const pd: PdJson.Pd = {
-            arrays: {},
-            patches: {
-                '0': {
-                    id: '0',
-                    args: [10],
-                    layout: {
-                        x: 778,
-                        y: 17,
-                        width: 450,
-                        height: 300,
-                    },
-                    nodes: {
-                        '0': {
-                            id: '0',
-                            proto: 'loadbang',
-                            args: [],
-                            layout: {
-                                x: 14,
-                                y: 13,
-                            },
-                        },
-                        '1': {
-                            id: '1',
-                            proto: 'print',
-                            args: ['bla'],
-                            layout: {
-                                x: 14,
-                                y: 34,
-                            },
-                        },
-                    },
-                    connections: [
-                        {
-                            source: { id: '0', port: 0 },
-                            sink: { id: '1', port: 0 },
-                        },
-                    ],
-                },
-            },
-        }
+    
+    it('should succeed rendering simple patch', () => {
+        const pd = parse(TEST_PATCHES.simple)
         assert.deepEqual(
             render(pd).split(NEWLINE_REGEX),
             TEST_PATCHES.simple.split(NEWLINE_REGEX)
+        )
+    })
+
+    it('should succeed rendering subpatches', () => {
+        const pd = parse(TEST_PATCHES.subpatches)
+        assert.deepEqual(
+            render(pd).split(NEWLINE_REGEX),
+            TEST_PATCHES.subpatches.split(NEWLINE_REGEX)
+        )
+    })
+
+    it('should succeed rendering subpatches', () => {
+        const pd = parse(TEST_PATCHES.nodeElems)
+        assert.deepEqual(
+            render(pd).split(NEWLINE_REGEX),
+            TEST_PATCHES.nodeElems.split(NEWLINE_REGEX)
         )
     })
 })
--- src/render.ts
+++ src/render.ts
@@ -16,10 +16,11 @@ const _nodesSort = (n1: PdJson.GenericNode, n2: PdJson.GenericNode): number =>
     parseFloat(n1.id) - parseFloat(n2.id)
 
 export default (pd: PdJson.Pd): Pd.PdString => {
-    return renderPatch(pd.patches['0'])
+    return renderPatch(pd, pd.patches['0'], true)
 }
 
-const renderPatch = (patch: PdJson.Patch, root = false): Pd.PdString => {
+const renderPatch = (pd: PdJson.Pd, patch: PdJson.Patch, root: boolean): Pd.PdString => {
+    const renderNodeCurried = renderNode.bind(this, pd)
     let rendered: Pd.PdString = ''
     const l = defaults({}, patch.layout, {
         x: 0,
@@ -30,52 +31,81 @@ const renderPatch = (patch: PdJson.Patch, root = false): Pd.PdString => {
     const a = patch.args
 
     rendered += `#N canvas ${l.x} ${l.y} ${l.width} ${l.height} ${a[0]}${
-        isUndefined(l.openOnLoad) ? '' : ' ' + l.openOnLoad
+        isUndefined(l.openOnLoad) ? '' : ' ' + (+l.openOnLoad)
     };\n`
     rendered += Object.values(patch.nodes)
         .sort(_nodesSort)
-        .map(renderNode)
+        .map(renderNodeCurried)
         .join('')
     rendered += patch.connections.map(renderConnection).join('')
 
     return rendered
 }
 
-const renderNode = (node: PdJson.GenericNode): Pd.PdString => {
-    const l = defaults({}, node.layout, { x: 0, y: 0 })
+const renderNode = (pd: PdJson.Pd, node: PdJson.GenericNode): Pd.PdString => {
+    const l = defaults(node.layout, { x: 0, y: 0 })
     const a = node.args
+    
+    if (node.proto === 'pd') {
+        let rendered: Pd.PdString = ''
+        rendered += renderPatch(pd, pd.patches[node.refId], false)
+        rendered += `#X restore ${l.x} ${l.y} pd ${a[0]};\n`
+        return rendered
+    }
+
+    const renderedControl = renderControl(node as PdJson.ControlNode)
+    if (renderedControl) {
+        return renderedControl
+    }
+    
     return `#X obj ${l.x} ${l.y} ${node.proto}${
         a.length ? ' ' + a.join(' ') : ''
     };\n`
 }
 
+const renderControl = (node: PdJson.ControlNode): Pd.PdString | null => {
+    const a = node.args
+    if (node.proto === 'floatatom' || node.proto === 'symbolatom') {
+        return `#X ${node.proto} ${node.layout.x} ${node.layout.y} ${node.layout.width} ${a[0]} ${a[1]} ${node.layout.labelPos} ${node.layout.label} ${a[2]} ${a[3]};\n`
+    } else if (node.proto === 'bng') {
+        return `#X obj ${node.layout.x} ${node.layout.y} bng ${node.layout.size} ${node.layout.hold} ${node.layout.interrupt} ${a[0]} ${a[1]} ${a[2]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.fgColor} ${node.layout.labelColor};\n`
+    } else if (node.proto === 'nbx') {
+        return `#X obj ${node.layout.x} ${node.layout.y} nbx ${node.layout.size} ${node.layout.height} ${a[0]} ${a[1]} ${node.layout.log} ${a[2]} ${a[3]} ${a[4]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.fgColor} ${node.layout.labelColor} ${node.layout.logHeight};\n`
+    } else if (node.proto === 'vsl' || node.proto === 'hsl') {
+        return `#X obj ${node.layout.x} ${node.layout.y} ${node.proto} ${node.layout.width} ${node.layout.height} ${a[0]} ${a[1]} ${node.layout.log} ${a[2]} ${a[3]} ${a[4]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.fgColor} ${node.layout.labelColor} ${a[5]} ${node.layout.steadyOnClick};\n`
+    } else if (node.proto === 'vradio' || node.proto === 'hradio') {
+        return `#X obj ${node.layout.x} ${node.layout.y} ${node.proto} ${node.layout.size} ${a[0]} ${a[1]} ${a[2]} ${a[3]} ${a[4]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.fgColor} ${node.layout.labelColor} ${a[5]};\n`
+    } else if (node.proto === 'vu') {
+        return `#X obj ${node.layout.x} ${node.layout.y} vu ${node.layout.width} ${node.layout.height} ${a[0]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.labelColor} ${node.layout.log} ${a[1]};\n`
+    } else if (node.proto === 'cnv') {
+        return `#X obj ${node.layout.x} ${node.layout.y} cnv ${node.layout.size} ${node.layout.width} ${node.layout.height} ${a[0]} ${a[1]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.labelColor} ${a[2]};\n`
+    }
+    return null
+}
+
 const renderConnection = ({ source, sink }: PdJson.Connection): Pd.PdString =>
     `#X connect ${source.id} ${source.port} ${sink.id} ${sink.port};\n`
 
-// var canvasTpl =
-//         '#N canvas {{{layout.x}}} {{{layout.y}}} {{{layout.width}}} {{{layout.height}}} {{{args.0}}}{{#layout.openOnLoad}} {{{.}}}{{/layout.openOnLoad}}',
-//     connectTpl =
-//         '#X connect {{{source.id}}} {{{source.port}}} {{{sink.id}}} {{{sink.port}}}'
 
 // var floatAtomTpl =
-//         '#X floatatom {{{layout.x}}} {{{layout.y}}} {{{layout.width}}} {{{args.0}}} {{{args.1}}} {{{layout.labelPos}}} {{{layout.label}}} {{{args.2}}} {{{args.3}}}',
+//         '#X floatatom ${node.layout.x} ${node.layout.y} ${node.layout.width} ${a[0]} ${a[1]} ${node.layout.labelPos} ${node.layout.label} ${a[2]} ${a[3]}',
 //     symbolAtomTpl =
-//         '#X symbolatom {{{layout.x}}} {{{layout.y}}} {{{layout.width}}} {{{args.0}}} {{{args.1}}} {{{layout.labelPos}}} {{{layout.label}}} {{{args.2}}} {{{args.3}}}',
+//         '#X symbolatom ${node.layout.x} ${node.layout.y} ${node.layout.width} ${a[0]} ${a[1]} ${node.layout.labelPos} ${node.layout.label} ${a[2]} ${a[3]}',
 //     bngTpl =
-//         '#X obj {{{layout.x}}} {{{layout.y}}} bng {{{layout.size}}} {{{layout.hold}}} {{{layout.interrupt}}} {{{args.0}}} {{{args.1}}} {{{args.2}}} {{{layout.label}}} {{{layout.labelX}}} {{{layout.labelY}}} {{{layout.labelFont}}} {{{layout.labelFontSize}}} {{{layout.bgColor}}} {{{layout.fgColor}}} {{{layout.labelColor}}}',
+//         '#X obj ${node.layout.x} ${node.layout.y} bng ${node.layout.size} ${node.layout.hold} ${node.layout.interrupt} ${a[0]} ${a[1]} ${a[2]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.fgColor} ${node.layout.labelColor}',
 //     nbxTpl =
-//         '#X obj {{{layout.x}}} {{{layout.y}}} nbx {{{layout.size}}} {{{layout.height}}} {{{args.0}}} {{{args.1}}} {{{layout.log}}} {{{args.2}}} {{{args.3}}} {{{args.4}}} {{{layout.label}}} {{{layout.labelX}}} {{{layout.labelY}}} {{{layout.labelFont}}} {{{layout.labelFontSize}}} {{{layout.bgColor}}} {{{layout.fgColor}}} {{{layout.labelColor}}} {{{layout.logHeight}}}',
+//         '#X obj ${node.layout.x} ${node.layout.y} nbx ${node.layout.size} ${node.layout.height} ${a[0]} ${a[1]} ${node.layout.log} ${a[2]} ${a[3]} ${a[4]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.fgColor} ${node.layout.labelColor} ${node.layout.logHeight}',
 //     vslTpl =
-//         '#X obj {{{layout.x}}} {{{layout.y}}} vsl {{{layout.width}}} {{{layout.height}}} {{{args.0}}} {{{args.1}}} {{{layout.log}}} {{{args.2}}} {{{args.3}}} {{{args.4}}} {{{layout.label}}} {{{layout.labelX}}} {{{layout.labelY}}} {{{layout.labelFont}}} {{{layout.labelFontSize}}} {{{layout.bgColor}}} {{{layout.fgColor}}} {{{layout.labelColor}}} {{{args.5}}} {{{layout.steadyOnClick}}}',
+//         '#X obj ${node.layout.x} ${node.layout.y} vsl ${node.layout.width} ${node.layout.height} ${a[0]} ${a[1]} ${node.layout.log} ${a[2]} ${a[3]} ${a[4]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.fgColor} ${node.layout.labelColor} ${a[5]} ${node.layout.steadyOnClick}',
 //     hslTpl =
-//         '#X obj {{{layout.x}}} {{{layout.y}}} hsl {{{layout.width}}} {{{layout.height}}} {{{args.0}}} {{{args.1}}} {{{layout.log}}} {{{args.2}}} {{{args.3}}} {{{args.4}}} {{{layout.label}}} {{{layout.labelX}}} {{{layout.labelY}}} {{{layout.labelFont}}} {{{layout.labelFontSize}}} {{{layout.bgColor}}} {{{layout.fgColor}}} {{{layout.labelColor}}} {{{args.5}}} {{{layout.steadyOnClick}}}',
+//         '#X obj ${node.layout.x} ${node.layout.y} hsl ${node.layout.width} ${node.layout.height} ${a[0]} ${a[1]} ${node.layout.log} ${a[2]} ${a[3]} ${a[4]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.fgColor} ${node.layout.labelColor} ${a[5]} ${node.layout.steadyOnClick}',
 //     vradioTpl =
-//         '#X obj {{{layout.x}}} {{{layout.y}}} vradio {{{layout.size}}} {{{args.0}}} {{{args.1}}} {{{args.2}}} {{{args.3}}} {{{args.4}}} {{{layout.label}}} {{{layout.labelX}}} {{{layout.labelY}}} {{{layout.labelFont}}} {{{layout.labelFontSize}}} {{{layout.bgColor}}} {{{layout.fgColor}}} {{{layout.labelColor}}} {{{args.5}}}',
+//         '#X obj ${node.layout.x} ${node.layout.y} vradio ${node.layout.size} ${a[0]} ${a[1]} ${a[2]} ${a[3]} ${a[4]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.fgColor} ${node.layout.labelColor} ${a[5]}',
 //     hradioTpl =
-//         '#X obj {{{layout.x}}} {{{layout.y}}} hradio {{{layout.size}}} {{{args.0}}} {{{args.1}}} {{{args.2}}} {{{args.3}}} {{{args.4}}} {{{layout.label}}} {{{layout.labelX}}} {{{layout.labelY}}} {{{layout.labelFont}}} {{{layout.labelFontSize}}} {{{layout.bgColor}}} {{{layout.fgColor}}} {{{layout.labelColor}}} {{{args.5}}}',
+//         '#X obj ${node.layout.x} ${node.layout.y} hradio ${node.layout.size} ${a[0]} ${a[1]} ${a[2]} ${a[3]} ${a[4]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.fgColor} ${node.layout.labelColor} ${a[5]}',
 //     vuTpl =
-//         '#X obj {{{layout.x}}} {{{layout.y}}} vu {{{layout.width}}} {{{layout.height}}} {{{args.0}}} {{{layout.label}}} {{{layout.labelX}}} {{{layout.labelY}}} {{{layout.labelFont}}} {{{layout.labelFontSize}}} {{{layout.bgColor}}} {{{layout.labelColor}}} {{{layout.log}}} {{{args.1}}}',
+//         '#X obj ${node.layout.x} ${node.layout.y} vu ${node.layout.width} ${node.layout.height} ${a[0]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.labelColor} ${node.layout.log} ${a[1]}',
 //     cnvTpl =
-//         '#X obj {{{layout.x}}} {{{layout.y}}} cnv {{{layout.size}}} {{{layout.width}}} {{{layout.height}}} {{{args.0}}} {{{args.1}}} {{{layout.label}}} {{{layout.labelX}}} {{{layout.labelY}}} {{{layout.labelFont}}} {{{layout.labelFontSize}}} {{{layout.bgColor}}} {{{layout.labelColor}}} {{{args.2}}}',
+//         '#X obj ${node.layout.x} ${node.layout.y} cnv ${node.layout.size} ${node.layout.width} ${node.layout.height} ${a[0]} ${a[1]} ${node.layout.label} ${node.layout.labelX} ${node.layout.labelY} ${node.layout.labelFont} ${node.layout.labelFontSize} ${node.layout.bgColor} ${node.layout.labelColor} ${a[2]}',
 //     objTpl =
-//         '#X obj {{{layout.x}}} {{{layout.y}}} {{{proto}}}{{#args}} {{.}}{{/args}}'
+//         '#X obj ${node.layout.x} ${node.layout.y} {{{proto}}}{{#args}} {{.}}{{/args}}'
